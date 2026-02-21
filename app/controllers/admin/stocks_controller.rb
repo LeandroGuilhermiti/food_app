fa3cf06ef8c83ad83707a46a7b1c49a30657b991
Leandro Guilhermiti <lguilhermiti@gmail.com>
2026-02-21 19:28:19 -0300

    funcionalidade de estoque

--- app/controllers/admin/stocks_controller.rb
+++ app/controllers/admin/stocks_controller.rb
@@ -56,7 +56,7 @@ class Admin::StocksController < AdminController
     @admin_stock.destroy!
 
     respond_to do |format|
-      format.html { redirect_to admin_product_stocks_path, notice: "Stock was successfully destroyed.", status: :see_other }
+      format.html { redirect_to admin_product_stocks_url, notice: "Stock was successfully destroyed.", status: :see_other }
       format.json { head :no_content }
     end
   end
