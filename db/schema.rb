27fe30ba242e7b7c64570adf81334008d5ca8430
Leandro Guilhermiti <lguilhermiti@gmail.com>
2026-02-21 20:13:58 -0300

    Gerenciamento de Vendas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_19_231442) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_21_230606) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -58,6 +58,26 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_19_231442) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "order_products", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.integer "order_id", null: false
+    t.integer "product_id", null: false
+    t.integer "quantity"
+    t.string "size"
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_products_on_order_id"
+    t.index ["product_id"], name: "index_order_products_on_product_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.string "customer_email"
+    t.boolean "fullfiled"
+    t.integer "total"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.boolean "active"
     t.integer "category_id", null: false
@@ -80,6 +100,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_19_231442) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "order_products", "orders"
+  add_foreign_key "order_products", "products"
   add_foreign_key "products", "categories"
   add_foreign_key "stocks", "products"
 end
